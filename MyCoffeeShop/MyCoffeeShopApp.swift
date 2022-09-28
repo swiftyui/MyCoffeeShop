ad935e28cecaef17b7dbef24f204e79a6710ae19
Arno van Zyl <vanzylarno@yahoo.com>
2022-09-28 15:32:18 +0200

    Initial Commit

---
+++ MyCoffeeShop/MyCoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoffeeShopApp.swift
+//  MyCoffeeShop
+//
+//  Created by Arno van Zyl on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct MyCoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
